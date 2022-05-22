47ba03bff0d78c8169c74defbf04f2702aa8f048
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-05-22 11:42:55 -0700

    add code for the API call

--- src/index.js
+++ src/index.js
@@ -2,17 +2,30 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import Triangle from './js/triangle.js';
 
-// $(function () {
-//   $('#triangle-checker-form').on("submit", function (event) {
-//     event.preventDefault();
-//     console.log('hello');
-//     const length1 = parseInt($('#length1').val());
-//     const length2 = parseInt($('#length2').val());
-//     const length3 = parseInt($('#length3').val());
-//     const triangle = new Triangle(length1, length2, length3);
-//     const response = triangle.checkType();
-//     $('#response').append(`<p>${response}</p>`);
-//   });
+$(document).ready(function () {
+  $('#weatherLocation').click(function () {
+    const city = $('#location').val();
+    $('#location').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=52bc74e156e2b353f19ce42357362bd7
+    `;
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }
+  });
+});
 
